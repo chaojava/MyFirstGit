f2bcc5ad52ad808c36d50c05aa8f468a27170385
chaojava <960323784@qq.com>
2018-05-04 23:56:51 +0800

    aaa

--- src/main/java/git_tes/com/git/Student.java
+++ src/main/java/git_tes/com/git/Student.java
@@ -4,9 +4,6 @@ public class Student {
 
 	private String name;
 	private String age;
-<<<<<<< HEAD
 	private String secode;
-=======
->>>>>>> branch 'master' of https://github.com/chaojava/MyFirstGit.git
 	private String first;
 }
