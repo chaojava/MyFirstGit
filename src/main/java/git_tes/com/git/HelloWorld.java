a2a91c6f9d108d3ad867b8acdc46da9e65449d15
chaojava <960323784@qq.com>
2018-05-04 23:22:29 +0800

    哇哇哇超

--- src/main/java/git_tes/com/git/HelloWorld.java
+++ src/main/java/git_tes/com/git/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	
 	public static void main(String[] args) {
 		System.out.println("开始一个git提交项目！");
+		System.out.println("我是工程一！");
 		System.out.println("我是工程二！");
 	}
 }
