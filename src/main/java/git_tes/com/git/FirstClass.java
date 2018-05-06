4690fce615713da03e5f8f285bfbcf7a50ae22d2
chaojava <960323784@qq.com>
2018-05-07 00:35:31 +0800

    0507

--- src/main/java/git_tes/com/git/FirstClass.java
+++ src/main/java/git_tes/com/git/FirstClass.java
@@ -1,5 +1,41 @@
 package git_tes.com.git;
 
+import java.util.Date;
+
 public class FirstClass {
 
+	
+	private String no;
+	private String className;
+	private int number;
+	private Date updateDate;
+	
+	
+	public String getNo() {
+		return no;
+	}
+	public void setNo(String no) {
+		this.no = no;
+	}
+	public String getClassName() {
+		return className;
+	}
+	public void setClassName(String className) {
+		this.className = className;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+	
+	
+	
 }
