bc8c452601411d59da709080c605c1d12b0fb689
chaojava <960323784@qq.com>
2018-05-07 23:17:14 +0800

    sssssaaaaa

--- src/main/java/git_tes/com/git/FirstClass.java
+++ src/main/java/git_tes/com/git/FirstClass.java
@@ -42,6 +42,8 @@ public class FirstClass {
 	
 	public void getNamedd(){
 		System.out.println("12222wsq");
+		System.out.println("111111111111");
+		System.out.println("1111111112133331111");
 	}
 	
 	
